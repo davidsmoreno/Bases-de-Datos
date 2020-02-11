517c073c0994ed2e4c5e352a4d519c7914c409ee
davidsmoreno <davidsantiagomoreno@gmail.com>
2020-02-11 12:59:10 -0500

    Create BOT.cpp
    vdvfd

---
+++ BOT.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class BOT{
+private:
+  BO *tuples;
+  vector<string> fields;
+  int num_r;
+
+  struct BO{
+    int id;
+
+
+    struct BR{
+      vector<string> fields
+      vector<string> fieldValues;
+
+
+
+    };
+
+  };
+
+public:
+
+
+
+
+
+
+
+};
+
+
+
