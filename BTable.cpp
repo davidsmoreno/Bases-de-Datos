a75ee2ef5167391539373ae0b89ec75fe4a4a71b
SaraPalacios <38820916+SaraPalacios@users.noreply.github.com>
2020-02-15 19:09:10 -0500

    Update BTable.cpp

--- BTable.cpp
+++ BTable.cpp
@@ -1,46 +1,37 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include"BOandBR.hpp"
+#include"BTable.hpp"
 
-using namespace std;
-
-class BTable{
-public:
-    int numObjects;
-    vector<string> fields;
-    vector<BRegister> registers;
-
-    BTable(){
-        numObjects = 0;
+BTable::BTable(){numObjects = 0;}
+BTable::BTable(vector<string> Fields){
+    numObjects = 0;
+    for(unsigned int i = 0; i<Fields.size();i++){
+        fields.push_back(Fields[i]);
     }
-
-    void addRegister(vector<string> values){
-        BRegister registro(numObjects);
-        if(values.size() != fields.size())
-            cout<<"BTable Error: There are no values to insert.";
-        else{
-            for(unsigned int i = 0; i<values.size();i++){
-                registro.addField(fields[i]);
-                registro.setFieldValue(fields[i], values[i]);
-            }
-        registers.push_back(registro);
-        numObjects++;
+}
+void BTable::addRegister(vector<string> values){
+    BRegister registro(numObjects);
+    if(values.size() != fields.size())
+        cout<<"BTable Error: There are no values to insert.";
+    else{
+        for(unsigned int i = 0; i<values.size();i++){
+            registro.addField(fields[i]);
+            registro.setFieldValue(fields[i], values[i]);
         }
+    registers.push_back(registro);
+    numObjects++;
     }
+}
 
-    void printTable(){
-        if(numObjects == 0)
-            cout<<"BTable Error: Table is empty.";
-        else{
-            for(unsigned int i = 0; i< fields.size();i++){
-                cout<<fields[i]<<'\t';
-            }
+void BTable::printTable(){
+    if(numObjects == 0)
+        cout<<"BTable Error: Table is empty.";
+    else{
+        for(unsigned int i = 0; i< fields.size();i++){
+            cout<<fields[i]<<'\t';
+        }
+        cout<<'\n';
+        for(unsigned int j = 0;j < registers.size();j++){
+            registers[j].printRegister;
             cout<<'\n';
-            for(unsigned int j = 0;j < registers.size();j++){
-                registers[j].printRegister;
-                cout<<'\n';
-            }
         }
     }
-};
+}
