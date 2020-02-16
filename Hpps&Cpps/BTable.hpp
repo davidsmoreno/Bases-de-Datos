9a609665871a4c1dcb23abaca1558393cb4befa4
Sara <saris_pal@hotmail.com>
2020-02-15 19:51:37 -0500

    Ordenando clases

---
+++ Hpps&Cpps/BTable.hpp
@@ -0,0 +1,25 @@
+#ifndef _BTable_hpp_
+#define _BTable_hpp_
+
+#include"BOandBR.hpp"
+
+using namespace std;
+
+class BTable{
+public:
+    int numObjects;
+    vector<string> fields;
+    vector<BRegister> registers;
+
+    BTable();
+    BTable(vector<string> Fields);
+
+    void addRegister(vector<string> values);
+    void printTable();
+    vector<string> getFieldsVals(vector<string> flds);
+    vector<string> getFieldsWhereEquals(vector<string> otherFields);
+    vector<string> getFieldsWhereComparison(vector<string> otherFields, char relation);
+    BTable crossProduct(BTable other);
+    BTable NaturalJoin(BTable other);
+};
+#endif
---
+++ boJunto.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class BO{
+public:
+    int id;
+
+    BO(){id = 0;}
+    BO(int idx){id = idx;}
+
+
+    int compareTo(BO bo){
+      if (id < bo.id)
+        return 1;
+      else if(id > bo.id)
+        return -1;
+      else
+        return 0;
+    }
+    int compareTo(int idx){
+      if (id < idx)
+        return 1;
+      else if(id > idx)
+        return -1;
+      else
+        return 0;
+    }
+};
+
+class BRegister : public BO{
+public:
+    vector<string> fields;
+    vector<string> fieldsValues;
+
+
+    BRegister():BO(){
+        fields.push_back("ID");
+        fieldsValues.push_back(to_string(BO::id));
+    }
+    BRegister(int idx):BO(idx){
+        fields.push_back("ID");
+        fieldsValues.push_back(to_string(BO::id));
+    }
+
+    int fieldIndex(string field){
+        int idx = -1;
+        for (unsigned int i = 0;i<fields.size();i++){
+            if(fields[i] == field){
+                idx = i;
+            }
+        }
+        return idx;
+    }
+
+    void addField(string newField){
+        if(fieldIndex(newField)!=-1)
+            cout<<"Register Error: Field already exists";
+        else{
+            fields.push_back(newField);
+        }
+    }
+
+    void setFieldValue(string value, string Field){
+        int idx = fieldIndex(Field);
+        if(idx == -1)
+            cout<<"Register Error: Field doesn't exist.";
+        else{
+            fieldsValues[idx] = value;
+        }
+
+    }
+    string getFieldValue(string Field){//en este field retorno el value
+        if(fieldIndex(Field)== -1)
+            return "Register error: Field not found.";
+        else
+        {
+            return fieldsValues[fieldIndex(Field)];
+        }
+        
+    }
+    string getRegister(){
+        string content = " ";
+        if(fieldsValues.size() == 0)
+            cout<<"Register Error: empty register.";
+        else{
+            for(unsigned int i = 0;i<fieldsValues.size();i++ ){
+                content += fieldsValues[i] + ",";
+            }
+        }
+        return content;
+    }
+
+};
+
+class BTable{
+public:
+    int numObjects;
+    vector<string> fields;
+    vector<BRegister> registers;
+
+    BTable(){
+        numObjects = 0;
+    }
+
+    void addRegister(vector<string> values){
+        BRegister registro(numObjects);
+        if(values.size() != fields.size())
+            cout<<"BTable Error: There are no values to insert.";
+        else{
+            for(unsigned int i = 0; i<values.size();i++){
+                registro.addField(fields[i]);
+                registro.setFieldValue(fields[i], values[i]);
+            }
+        registers.push_back(registro);
+        numObjects++;
+        }
+    }
+
+    
+};
